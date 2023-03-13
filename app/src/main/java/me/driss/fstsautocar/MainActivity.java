76dd6ebcf6bbd09767cf60c860633d4f570941a5
Dris7 <drissatkou7@gmail.com>
2023-03-13 23:27:00 +0100

    second Version

--- app/src/main/java/me/driss/fstsautocar/MainActivity.java
+++ app/src/main/java/me/driss/fstsautocar/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     private String deviceAddress="00:21:09:01:25:35";
     private BluetoothSocket btSocket = null;
 
-    @SuppressLint("MissingPermishsihhon")
+    @SuppressLint("MissingPermidshsihhon")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
